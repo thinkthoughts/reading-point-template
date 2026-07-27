9915bb76c580f2a81abee100234ace1c694e1903
dan_hawkley <consensusmechanics@gmail.com>
2026-07-27 13:42:16 -0600

    Update __init__.py

--- src/sensors_becker/__init__.py
+++ src/sensors_becker/__init__.py
@@ -1,6 +1,5 @@
 """Public package interface for sensors-becker."""
 
-from .context import RepositoryContext, build_context
 from .dialogue_renderer import (
     DialogueFigure,
     DialogueNode,
@@ -8,22 +7,13 @@ from .dialogue_renderer import (
     NotebookDialogueRenderer,
     render_dialogue,
 )
-from .runtime import NotebookRuntime, initialize_notebook
-from .validation import (
-    ContextValidationError,
-    validate_context,
-)
+from .runtime import initialize_notebook
 
 __all__ = [
-    "ContextValidationError",
     "DialogueFigure",
     "DialogueNode",
     "DialogueRelation",
     "NotebookDialogueRenderer",
-    "NotebookRuntime",
-    "RepositoryContext",
-    "build_context",
     "initialize_notebook",
     "render_dialogue",
-    "validate_context",
 ]
